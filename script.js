62a4cd18e8b4395fe89cc44fdae455eb36274029
6395728950 <161918761+6395728950@users.noreply.github.com>
2025-02-21 17:23:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 //complete this code
-class Animal {}
+class Animal {
+    constructor(private _species: string) {}
 
-class Dog extends Animal {}
+    get species(): string {
+        return this._species;
+    }
 
-class Cat extends Animal {}
+    makeSound(): void {
+        console.log(`The ${this._species} makes a sound`);
+    }
+}
+
+// Cat subclass extending Animal
+class Cat extends Animal {
+    constructor(species: string) {
+        super(species);
+    }
+
+    purr(): void {
+        console.log("purr");
+    }
+}
+
+// Dog subclass extending Animal
+class Dog extends Animal {
+    constructor(species: string) {
+        super(species);
+    }
+
+    bark(): void {
+        console.log("woof");
+    }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
